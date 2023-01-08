8b649f291d7d5e1a8b56ea17db21968a388df106
Eduardo Celino <ehcelino@gmail.com>
2023-01-08 16:17:50 -0300

    antes partial

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+  
   # GET /carts or /carts.json
   def index
     @carts = Cart.all
@@ -49,10 +51,11 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to root_url, notice: "Seu carrinho está vazio." }
       format.json { head :no_content }
     end
   end
@@ -67,4 +70,10 @@ class CartsController < ApplicationController
     def cart_params
       params.fetch(:cart, {})
     end
+
+    def invalid_cart
+      logger.error "Tentativa de acessar carrinho de compras inválido #{params[:id]}"
+      redirect_to root_url, notice: "Carrinho inválido"
+    end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,24 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params.reject { |k| k["images"] })
+      if product_params[:images].present?
+        product_params[:images].each do |image|
+          @product.images.attach(image)
+        end
+      end
+      return redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
