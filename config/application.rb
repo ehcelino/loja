c854591d2d3e41716ed357f0e3b311b4ac4a679a
Eduardo Celino <ehcelino@gmail.com>
2023-01-13 19:44:46 -0300

    Nova config no application.rb para render

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Loja
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # teste
+    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
   end
 end
