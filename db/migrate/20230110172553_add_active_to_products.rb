ecf52f2398d2400373002b4d6e6a1c5cc8979dbc
Eduardo Celino <ehcelino@gmail.com>
2023-01-13 21:20:58 -0300

    coluna active agora é inteiro

--- db/migrate/20230110172553_add_active_to_products.rb
+++ db/migrate/20230110172553_add_active_to_products.rb
@@ -1,5 +1,5 @@
 class AddActiveToProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :products, :active, :boolean, default: true
+    add_column :products, :active, :integer, default: 1
   end
 end
