12cd9007e6c4f16688f2dc6881ab7bf2105dda2c
Eduardo Celino <ehcelino@gmail.com>
2023-01-13 21:29:30 -0300

    removida migration do campo active

--- db/migrate/20230110221248_change_active_type_on_products.rb
+++
@@ -1,8 +0,0 @@
-class ChangeActiveTypeOnProducts < ActiveRecord::Migration[7.0]
-  def up
-    change_column :products, :active, :integer, default: 1
-  end
-  def down
-    change_column :products, :active, :boolean, default: true
-  end
-end
