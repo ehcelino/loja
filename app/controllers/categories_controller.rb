4ad4d615778f07c6419b3f1ce1052222b7c08629
Eduardo Celino <ehcelino@gmail.com>
2023-01-13 22:07:59 -0300

    mais uma correção para categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,4 +12,19 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to new_category_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category)
+  end
+
 end
