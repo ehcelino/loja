1f55a99b033a46283f75ae3d9495f3463520da16
Eduardo Celino <ehcelino@gmail.com>
2023-01-16 20:34:34 -0300

    correção

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,7 +1,18 @@
 class SalesController < ApplicationController
-
+  
+  # after_action :write_file
+  
   def show
     @sale = Sale.find(params[:id])
   end
+
+
+  # protected
+
+  # def write_file
+  #   File.open('public/test.html', 'w+') do |f|
+  #     f.write response.body
+  #   end
+  # end
 
 end
